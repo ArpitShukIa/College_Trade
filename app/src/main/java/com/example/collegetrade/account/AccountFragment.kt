c3682c68b1c60d88c33adfd68509edf12db1b338
Arpit Shukla <arpitshukla62@gmail.com>
2020-06-27 17:11:00 +0530

    Enable logout button

--- app/src/main/java/com/example/collegetrade/account/AccountFragment.kt
+++ app/src/main/java/com/example/collegetrade/account/AccountFragment.kt
@@ -1,23 +1,52 @@
 package com.example.collegetrade.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
 import com.example.collegetrade.R
+import com.example.collegetrade.SplashScreenActivity
+import com.example.collegetrade.databinding.FragmentAccountBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-/**
- * A simple [Fragment] subclass.
- */
 class AccountFragment : Fragment() {
+
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+
+        binding.logoutLayout.setOnClickListener {
+            handleLogout()
+        }
+
+        return binding.root
+    }
+
+    private fun handleLogout() {
+        MaterialDialog(requireContext())
+            .show {
+                title(R.string.are_you_sure)
+                positiveButton(R.string.logout) {
+                    Firebase.auth.signOut()
+                    startActivity(Intent(activity, SplashScreenActivity::class.java))
+                    requireActivity().finish()
+                }
+                negativeButton(R.string.cancel)
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
